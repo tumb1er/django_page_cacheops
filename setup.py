5dcb2bcdc7a16a368cc839b93d14f834828cdb33
Sergey Tikhonov <zimbler@gmail.com>
2013-05-08 17:28:21 +0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='django_page_cacheops',
+    version='1.0',
+    packages=['cacheops_pages'],
+    url='https://github.com/tumb1er/django_page_cacheops',
+    license='beer licence',
+    author='tumbler',
+    author_email='stikhonov@rutube.ru',
+    description='Caches pages with cacheops invalidation'
+)
